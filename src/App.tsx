2521df111cec95afc5e2851924b294071505fd76
Andrew Smith <meuzishun@gmail.com>
2024-10-18 08:22:42 -0400

    Install TanStack Query and wrap app with provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Provider as StoreProvider } from 'react-redux';
 import store from '~/store';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppRoutes from '~/routes';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <StoreProvider store={store}>
-      <AppRoutes />
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+      </QueryClientProvider>
     </StoreProvider>
   );
 }
