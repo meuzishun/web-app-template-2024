c9703ffd7a97d09d6216c6d423ae66048774632c
Andrew Smith <meuzishun@gmail.com>
2024-10-25 10:43:12 -0400

    Change User type import to UserType

--- src/features/users/api/usersApi.ts
+++ src/features/users/api/usersApi.ts
@@ -1,19 +1,19 @@
 import { axiosInstance } from '~/api';
-import type { User } from '../types';
+import type { UserType } from '../types';
 
 const url = '/users';
 
-export const fetchUsers = async (): Promise<User[]> => {
+export const fetchUsers = async (): Promise<UserType[]> => {
   const response = await axiosInstance.get(url);
   return response.data;
 };
 
-export const createUser = async (newUser: Omit<User, 'id'>) => {
+export const createUser = async (newUser: Omit<UserType, 'id'>) => {
   const response = await axiosInstance.post(url, newUser);
   return response.data;
 };
 
-export const updateUser = async (updatedUser: User) => {
+export const updateUser = async (updatedUser: UserType) => {
   const response = await axiosInstance.put(
     `${url}/${updatedUser.id}`,
     updatedUser
