cb8f230f52bef82332680a9c98c0f4859239e266
Andrew Smith <meuzishun@gmail.com>
2024-10-25 10:32:52 -0400

    Change Post type import to PostType

--- src/features/posts/api/postsApi.ts
+++ src/features/posts/api/postsApi.ts
@@ -1,19 +1,19 @@
 import { axiosInstance } from '~/api';
-import type { Post } from '../types';
+import type { PostType } from '../types';
 
 const url = '/posts';
 
-export const fetchPosts = async (): Promise<Post[]> => {
+export const fetchPosts = async (): Promise<PostType[]> => {
   const response = await axiosInstance.get(url);
   return response.data;
 };
 
-export const createPost = async (newPost: Omit<Post, 'id'>) => {
+export const createPost = async (newPost: Omit<PostType, 'id'>) => {
   const response = await axiosInstance.post(url, newPost);
   return response.data;
 };
 
-export const updatePost = async (updatedPost: Post) => {
+export const updatePost = async (updatedPost: PostType) => {
   const response = await axiosInstance.put(
     `${url}/${updatedPost.id}`,
     updatedPost
