a3705b485df02f76d1aa3d1324b612eeb7c4eefe
Andrew Smith <meuzishun@gmail.com>
2024-11-10 13:15:53 -0500

    Refactor HomePage
    Use MUI components to style and layout counter code in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,33 @@
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import Typography from '@mui/material/Typography';
+
 import { useCounter } from '~/features/counter';
 
 function HomePage() {
   const { counterValue, incrementCount, decrementCount } = useCounter();
 
   return (
-    <div>
-      <button onClick={incrementCount}>Increment</button>
-      {counterValue}
-      <button onClick={decrementCount}>Decrement</button>
-    </div>
+    <Stack
+      direction='row'
+      spacing={4}
+      sx={{
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <IconButton onClick={decrementCount} size='large' color='error'>
+        <RemoveCircleIcon />
+      </IconButton>
+      <Typography variant='h3' component='p' sx={{ display: 'inline' }}>
+        {counterValue}
+      </Typography>
+      <IconButton onClick={incrementCount} size='large' color='success'>
+        <AddCircleIcon />
+      </IconButton>
+    </Stack>
   );
 }
 
