85c55a15b0de742b8c3c2e75e5a30bd37de6c8df
Andrew Smith <meuzishun@gmail.com>
2024-11-10 15:00:55 -0500

    Add text to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 import IconButton from '@mui/material/IconButton';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
@@ -10,24 +11,33 @@ function HomePage() {
   const { counterValue, incrementCount, decrementCount } = useCounter();
 
   return (
-    <Stack
-      direction='row'
-      spacing={4}
-      sx={{
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <IconButton onClick={decrementCount} size='large' color='minus'>
-        <RemoveCircleIcon />
-      </IconButton>
-      <Typography variant='h3' component='p' sx={{ display: 'inline' }}>
-        {counterValue}
+    <Container>
+      <Typography variant='h6' align='center'>
+        What would a React demo be without the obligatory...
       </Typography>
-      <IconButton onClick={incrementCount} size='large' color='plus'>
-        <AddCircleIcon />
-      </IconButton>
-    </Stack>
+      <Typography variant='h2' align='center'>
+        Counter Component
+      </Typography>
+      <Stack
+        direction='row'
+        spacing={4}
+        sx={{
+          mt: 8,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <IconButton onClick={decrementCount} size='large' color='minus'>
+          <RemoveCircleIcon />
+        </IconButton>
+        <Typography variant='h3' component='p' sx={{ display: 'inline' }}>
+          {counterValue}
+        </Typography>
+        <IconButton onClick={incrementCount} size='large' color='plus'>
+          <AddCircleIcon />
+        </IconButton>
+      </Stack>
+    </Container>
   );
 }
 
