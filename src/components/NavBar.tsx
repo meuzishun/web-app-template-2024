d7ab47d2b82ce86ecb9fd94f3d3ddebfa964eb2c
Andrew Smith <meuzishun@gmail.com>
2024-11-25 05:21:32 -0500

    Extract NavBar from MainLayout into separate component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { NavLink } from 'react-router-dom';
+import { Stack, Typography } from '@mui/material';
+
+const navItems = [
+  { text: 'Home', path: '/' },
+  { text: 'About', path: '/about' },
+  { text: 'Contact', path: '/contact' },
+];
+
+const NavBar: React.FC = () => {
+  return (
+    <AppBar position='static' component='nav'>
+      <Stack
+        direction='row'
+        sx={{ justifyContent: 'space-between', alignItems: 'center', mx: 4 }}
+      >
+        <Typography variant='h4'>Web App Template</Typography>
+        <List sx={{ display: 'inline-flex' }}>
+          {navItems.map((item) => (
+            <ListItem key={item.text} disablePadding>
+              <ListItemButton
+                component={NavLink}
+                to={item.path}
+                sx={{ textAlign: 'center' }}
+              >
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Stack>
+    </AppBar>
+  );
+};
+
+export default NavBar;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,42 +1,11 @@
 import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import { NavLink, Outlet } from 'react-router-dom';
-import { Stack, Typography } from '@mui/material';
-
-const navItems = [
-  { text: 'Home', path: '/' },
-  { text: 'About', path: '/about' },
-  { text: 'Contact', path: '/contact' },
-];
+import { Outlet } from 'react-router-dom';
+import { NavBar } from '~/components';
 
 const MainLayout: React.FC = () => {
   return (
     <>
-      <AppBar position='static' component='nav'>
-        <Stack
-          direction='row'
-          sx={{ justifyContent: 'space-between', alignItems: 'center', mx: 4 }}
-        >
-          <Typography variant='h4'>Web App Template</Typography>
-          <List sx={{ display: 'inline-flex' }}>
-            {navItems.map((item) => (
-              <ListItem key={item.text} disablePadding>
-                <ListItemButton
-                  component={NavLink}
-                  to={item.path}
-                  sx={{ textAlign: 'center' }}
-                >
-                  <ListItemText primary={item.text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Stack>
-      </AppBar>
+      <NavBar />
       <Outlet />
     </>
   );
