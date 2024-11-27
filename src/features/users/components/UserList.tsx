9a6cc3ec18932e0fd15f1fd25304cea296c830f5
Andrew Smith <meuzishun@gmail.com>
2024-11-27 05:31:14 -0500

    Use LoadingIndicator in UserList

--- src/features/users/components/UserList.tsx
+++ src/features/users/components/UserList.tsx
@@ -3,16 +3,17 @@ import List from '@mui/material/List';
 import { UserType } from '../types';
 import { useUsers } from '../hooks';
 import User from './User';
+import { LoadingIndicator } from '~/components';
 
 const UserList: React.FC = () => {
-  const { data, isLoading, error } = useUsers();
+  const { data: users, isLoading, error } = useUsers();
 
-  if (isLoading) return <div>Loading users...</div>;
+  if (isLoading) return <LoadingIndicator />;
   if (error) return <div>Error loading users</div>;
 
   return (
     <List>
-      {data?.map((user: UserType) => (
+      {users?.map((user: UserType) => (
         <User key={user.id} user={user} />
       ))}
     </List>
