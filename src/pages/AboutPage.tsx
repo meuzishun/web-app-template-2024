149618c87fa9fe03cb59f73d03f082f1a9a65abe
Andrew Smith <meuzishun@gmail.com>
2024-11-28 09:59:01 -0500

    Include an about text on the AboutPage
    Store the actual text in the data dir and import it to the AboutPage

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,14 @@
+import { Typography } from '@mui/material';
 import Container from '@mui/material/Container';
+import { texts } from '~/data';
 
 function AboutPage() {
-  return <Container sx={{ mt: 8 }}></Container>;
+  return (
+    <Container sx={{ mt: 8 }}>
+      <Typography variant='h4'>About this template...</Typography>
+      <Typography variant='body1'>{texts.about}</Typography>
+    </Container>
+  );
 }
 
 export default AboutPage;
