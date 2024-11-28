62061b1b4487781e02ccf6f77a6f6f4a70d6e709
Andrew Smith <meuzishun@gmail.com>
2024-11-28 11:09:00 -0500

    Add margin to about heading

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -5,7 +5,9 @@ import { texts } from '~/data';
 function AboutPage() {
   return (
     <Container sx={{ mt: 8 }}>
-      <Typography variant='h4'>About this template...</Typography>
+      <Typography variant='h4' mb={2}>
+        About this template...
+      </Typography>
       <Typography variant='body1'>{texts.about}</Typography>
     </Container>
   );
